023f4d49723434ac72cdeb1ff38ad6890e24b92a
remorhaz <zoohie@gmail.com>
2018-02-17 12:21:30 +0300

    Regexp lexeme test added

---
+++ tests/RegExp/LexemeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Remorhaz\UniLex\Test\RegExp;
+
+use PHPUnit\Framework\TestCase;
+use Remorhaz\UniLex\RegExp\Lexeme;
+use Remorhaz\UniLex\RegExp\TokenType;
+use Remorhaz\UniLex\SymbolBuffer;
+use Remorhaz\UniLex\SymbolBufferLexemeInfo;
+
+class LexemeTest extends TestCase
+{
+
+    public function testGetType_ConstructWithValue_ReturnsSameValue(): void
+    {
+        $buffer = SymbolBuffer::fromString('a');
+        $lexemeInfo = new SymbolBufferLexemeInfo($buffer, 0, 1);
+        $lexeme = new Lexeme($lexemeInfo, TokenType::OTHER_HEX_LETTER, 0x61);
+        $actual = $lexeme->getType();
+        self::assertSame(TokenType::OTHER_HEX_LETTER, $actual);
+    }
+
+    public function testGetSymbol_ConstructWithValue_ReturnsSameValue(): void
+    {
+        $buffer = SymbolBuffer::fromString('a');
+        $lexemeInfo = new SymbolBufferLexemeInfo($buffer, 0, 1);
+        $lexeme = new Lexeme($lexemeInfo, TokenType::OTHER_HEX_LETTER, 0x61);
+        $actual = $lexeme->getSymbol();
+        self::assertSame(0x61, $actual);
+    }
+}
