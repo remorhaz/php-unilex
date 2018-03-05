d53dbe1d42ac92886b93be467527a24ce8c256ff
remorhaz <zoohie@gmail.com>
2018-03-06 01:40:57 +0300

    Token can store attributes

--- src/Token.php
+++ src/Token.php
@@ -13,6 +13,8 @@ class Token
 
     private $matcherInfo;
 
+    private $attributeList = [];
+
     public function __construct(int $type, bool $isEoi)
     {
         $this->type = $type;
@@ -47,5 +49,31 @@ class Token
     public function getMatcherInfo(): ?TokenMatcherInfoInterface
     {
         return $this->matcherInfo;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @throws Exception
+     */
+    public function setAttribute(string $name, $value): void
+    {
+        if (isset($this->attributeList[$name])) {
+            throw new Exception("Synthesized attribute {$name} is already set");
+        }
+        $this->attributeList[$name] = $value;
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     * @throws Exception
+     */
+    public function getAttribute(string $name)
+    {
+        if (!isset($this->attributeList[$name])) {
+            throw new Exception("Synthesized attribute {$name} is undefined");
+        }
+        return $this->attributeList[$name];
     }
 }
