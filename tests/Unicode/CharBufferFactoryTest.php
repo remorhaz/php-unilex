4dbef7d689bbfcce5ae332da8b47e8b64c010206
remorhaz <zoohie@gmail.com>
2018-03-08 00:15:42 +0300

    Unicode utility classes covered with tests

---
+++ tests/Unicode/CharBufferFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Remorhaz\UniLex\Test\Unicode;
+
+use PHPUnit\Framework\TestCase;
+use Remorhaz\UniLex\CharBuffer;
+use Remorhaz\UniLex\Unicode\CharBufferFactory;
+use Remorhaz\UniLex\Unicode\Utf8TokenMatcher;
+
+/**
+ * @covers \Remorhaz\UniLex\Unicode\CharBufferFactory
+ */
+class CharBufferFactoryTest extends TestCase
+{
+
+    public function testCreateFromBuffer_CreateFromBuffer_ResultGetSymbolReturnsUnicodeCharacter(): void
+    {
+        $source = CharBuffer::fromString('本');
+        $actualValue = CharBufferFactory::createFromBuffer($source, new Utf8TokenMatcher)->getSymbol();
+        self::assertSame(0x672C, $actualValue);
+    }
+
+    public function testCreateFromString_CreateFromBuffer_ResultGetSymbolReturnsUnicodeCharacter(): void
+    {
+        $actualValue = CharBufferFactory::createFromString('本', new Utf8TokenMatcher)->getSymbol();
+        self::assertSame(0x672C, $actualValue);
+    }
+
+    public function testCreateFromUtf8String_CreateFromBuffer_ResultGetSymbolReturnsUnicodeCharacter(): void
+    {
+        $actualValue = CharBufferFactory::createFromUtf8String('本')->getSymbol();
+        self::assertSame(0x672C, $actualValue);
+    }
+}
---
+++ tests/Unicode/Grammar/TokenFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Remorhaz\UniLex\Test\Unicode\Grammar;
+
+use PHPUnit\Framework\TestCase;
+use Remorhaz\UniLex\Unicode\Grammar\TokenFactory;
+use Remorhaz\UniLex\Unicode\Grammar\TokenType;
+
+class TokenFactoryTest extends TestCase
+{
+
+    public function testCreateEoiToken_Constructed_ResultIsEoiReturnsTrue(): void
+    {
+        $actualValue = (new TokenFactory)
+            ->createEoiToken()
+            ->isEoi();
+        self::assertTrue($actualValue);
+    }
+
+    public function testCreateToken_EoiId_ResultIsEoiReturnsTrue(): void
+    {
+        $actualValue = (new TokenFactory)
+            ->createToken(TokenType::EOI)
+            ->isEoi();
+        self::assertTrue($actualValue);
+    }
+
+    public function testCreateToken_NotEoiId_ResultIsEoiReturnsTrue(): void
+    {
+        $actualValue = (new TokenFactory)
+            ->createToken(TokenType::SYMBOL)
+            ->isEoi();
+        self::assertFalse($actualValue);
+    }
+}
