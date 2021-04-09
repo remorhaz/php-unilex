cbd3ed89140a8918f2f35cfb10557c5ec4ebbbc7
remorhaz <zoohie@gmail.com>
2021-04-09 14:46:44 +0300

    Loading lookup table test added

--- tests/Parser/LL1/ParserTest.php
+++ tests/Parser/LL1/ParserTest.php
@@ -6,7 +6,9 @@ use PHPUnit\Framework\TestCase;
 use Remorhaz\UniLex\Example\SimpleExpr\Grammar\ConfigFile;
 use Remorhaz\UniLex\Example\SimpleExpr\Grammar\TokenType;
 use Remorhaz\UniLex\Exception as UniLexException;
+use Remorhaz\UniLex\Grammar\ContextFree\GrammarInterface;
 use Remorhaz\UniLex\Grammar\ContextFree\GrammarLoader;
+use Remorhaz\UniLex\Grammar\ContextFree\Production;
 use Remorhaz\UniLex\Grammar\ContextFree\TokenFactory;
 use Remorhaz\UniLex\Parser\LL1\AbstractParserListener;
 use Remorhaz\UniLex\Parser\LL1\Parser;
@@ -27,7 +29,7 @@ class ParserTest extends TestCase
      * @throws UniLexException
      * @dataProvider providerValidGrammarInput
      */
-    public function testParse_ValidBuffer_OnTokenTriggeredForEachToken(string $configFile, array $input): void
+    public function testRun_ValidBuffer_OnTokenTriggeredForEachToken(string $configFile, array $input): void
     {
         $grammar = GrammarLoader::loadFile($configFile);
         $buffer = new CharBuffer(...$input);
@@ -47,7 +49,7 @@ class ParserTest extends TestCase
      * @throws UnexpectedTokenException
      * @throws UniLexException
      */
-    public function testParse_InvalidBuffer_ThrowsException(): void
+    public function testRun_InvalidBuffer_ThrowsException(): void
     {
         $grammar = GrammarLoader::loadFile(ConfigFile::getPath());
         $input = [TokenType::ID, TokenType::PLUS, TokenType::STAR];
@@ -65,7 +67,7 @@ class ParserTest extends TestCase
     /**
      * @throws UniLexException
      */
-    public function testParse_InvalidBuffer_ThrowsErrorInfoWithMatchingExpectedTokenTypeList(): void
+    public function testRun_InvalidBuffer_ThrowsErrorInfoWithMatchingExpectedTokenTypeList(): void
     {
         $grammar = GrammarLoader::loadFile(ConfigFile::getPath());
         $input = [TokenType::ID, TokenType::PLUS, TokenType::STAR];
@@ -87,21 +89,21 @@ class ParserTest extends TestCase
     /**
      * @throws UniLexException
      */
-    public function testParse_InvalidBuffer_ThrowsErrorInfoWithMatchingUnexpectedToken(): void
+    public function testRun_InvalidBuffer_ThrowsErrorInfoWithMatchingUnexpectedToken(): void
     {
         $grammar = GrammarLoader::loadFile(ConfigFile::getPath());
         $input = [TokenType::ID, TokenType::PLUS, TokenType::STAR];
         $buffer = new CharBuffer(...$input);
         $reader = new TokenReader($buffer, new TokenMatcherByType(), new TokenFactory($grammar));
-        $listener = $this->createMock(AbstractParserListener::class);
-        /** @var AbstractParserListener $listener */
+        $listener = $this->createStub(AbstractParserListener::class);
         $parser = new Parser($grammar, $reader, $listener);
         try {
             $parser->run();
+            $actualValue = null;
         } catch (UnexpectedTokenException $e) {
             $actualValue = $e->getErrorInfo()->getUnexpectedToken()->getType();
-            self::assertSame(TokenType::STAR, $actualValue);
         }
+        self::assertSame(TokenType::STAR, $actualValue);
     }
 
     public function providerValidGrammarInput(): array
@@ -121,5 +123,246 @@ class ParserTest extends TestCase
                 [ConfigFile::getPath(), $input];
         }
         return $data;
+    }
+
+    /**
+     * @throws UniLexException
+     */
+    public function testLoadLookupTable_FileDoesNotExist_ThrowsException(): void
+    {
+        $grammar = GrammarLoader::loadFile(ConfigFile::getPath());
+        $buffer = new CharBuffer();
+        $reader = new TokenReader($buffer, new TokenMatcherByType(), new TokenFactory($grammar));
+        $listener = $this->createStub(AbstractParserListener::class);
+        $parser = new Parser($grammar, $reader, $listener);
+        $this->expectException(UniLexException::class);
+        $this->expectExceptionMessageMatches(
+            '#Failed to load lookup table from file .+/NotExists\.php#',
+        );
+        $parser->loadLookupTable(__DIR__ . '/NotExists.php');
+    }
+
+    /**
+     * @throws UnexpectedTokenException
+     * @throws UniLexException
+     */
+    public function testRun_LookupTableNotLoaded_UsesGrammarToBuildNewTable(): void
+    {
+        /**
+         * The grammar is the following:
+         *
+         * Productions:
+         *   1 -> 2 3 (start production)
+         *   2 -> 4 (we need at least one non-terminal to access the table)
+         *   4 -> 5
+         *
+         * Terminals:
+         *   3 (EOI)
+         *   5
+         *
+         * Tokens (mapped to terminals):
+         *   3 -> 6 (EOI)
+         *   5 -> 7
+         */
+        $grammar = $this->createMock(GrammarInterface::class);
+        $grammar
+            ->method('getRootSymbol')
+            ->willReturn(1);
+        $grammar
+            ->method('getStartSymbol')
+            ->willReturn(2);
+        $grammar
+            ->method('getEoiSymbol')
+            ->willReturn(3);
+        $grammar
+            ->method('getEoiToken')
+            ->willReturn(6);
+        $grammar
+            ->method('isEoiToken')
+            ->willReturnMap(
+                [
+                    [6, true],
+                    [7, false],
+                ],
+            );
+        $grammar
+            ->method('isTerminal')
+            ->willReturnMap(
+                [
+                    [1, false],
+                    [2, false],
+                    [3, true],
+                    [4, false],
+                    [5, true],
+                ],
+            );
+        $grammar
+            ->method('getTerminalList')
+            ->willReturn([3, 5]);
+        $grammar
+            ->method('tokenMatchesTerminal')
+            ->willReturnMap(
+                [
+                    [3, 6, true],
+                    [5, 7, true],
+                ],
+            );
+        $grammar
+            ->method('getToken')
+            ->willReturnMap(
+                [
+                    [3, 6],
+                    [5, 7],
+                ],
+            );
+        $grammar
+            ->method('getProduction')
+            ->willReturnMap(
+                [
+                    [1, 0, new Production(1, 0, 2, 3)],
+                    [2, 0, new Production(2, 0, 4)],
+                    [4, 0, new Production(4, 0, 5)],
+                ],
+            );
+        $grammar
+            ->method('getProductionList')
+            ->willReturnMap(
+                [
+                    [1, [new Production(1, 0, 2, 3)]],
+                    [2, [new Production(2, 0, 4)]],
+                    [4, [new Production(4, 0, 5)]],
+                ],
+            );
+        $grammar
+            ->method('getFullProductionList')
+            ->willReturn(
+                [
+                    new Production(1, 0, 2, 3),
+                    new Production(2, 0, 4),
+                    new Production(4, 0, 5),
+                ],
+            );
+        $buffer = new CharBuffer(7);
+        $reader = new TokenReader($buffer, new TokenMatcherByType(), new TokenFactory($grammar));
+        $listener = $this->createStub(AbstractParserListener::class);
+        $parser = new Parser($grammar, $reader, $listener);
+        $grammar
+            ->expects(self::atLeastOnce())
+            ->method('getNonTerminalList')
+            ->willReturn([1, 2, 4]);
+        $parser->run();
+    }
+
+    /**
+     * @throws UnexpectedTokenException
+     * @throws UniLexException
+     */
+    public function testRun_LookUpTableLoaded_NeverUsesGrammarToBuildNewTable(): void
+    {
+        /**
+         * The grammar is the following:
+         *
+         * Productions:
+         *   1 -> 2 3 (start production)
+         *   2 -> 4 (we need at least one non-terminal to access the table)
+         *   4 -> 5
+         *
+         * Terminals:
+         *   3 (EOI)
+         *   5
+         *
+         * Tokens (mapped to terminals):
+         *   3 -> 6 (EOI)
+         *   5 -> 7
+         */
+        $grammar = $this->createMock(GrammarInterface::class);
+        $grammar
+            ->method('getRootSymbol')
+            ->willReturn(1);
+        $grammar
+            ->method('getStartSymbol')
+            ->willReturn(2);
+        $grammar
+            ->method('getEoiSymbol')
+            ->willReturn(3);
+        $grammar
+            ->method('getEoiToken')
+            ->willReturn(6);
+        $grammar
+            ->method('isEoiToken')
+            ->willReturnMap(
+                [
+                    [6, true],
+                    [7, false],
+                ],
+            );
+        $grammar
+            ->method('isTerminal')
+            ->willReturnMap(
+                [
+                    [1, false],
+                    [2, false],
+                    [3, true],
+                    [4, false],
+                    [5, true],
+                ],
+            );
+        $grammar
+            ->method('getTerminalList')
+            ->willReturn([3, 5]);
+        $grammar
+            ->method('tokenMatchesTerminal')
+            ->willReturnMap(
+                [
+                    [3, 6, true],
+                    [5, 7, true],
+                ],
+            );
+        $grammar
+            ->method('getToken')
+            ->willReturnMap(
+                [
+                    [3, 6],
+                    [5, 7],
+                ],
+            );
+        $grammar
+            ->method('getProduction')
+            ->willReturnMap(
+                [
+                    [1, 0, new Production(1, 0, 2, 3)],
+                    [2, 0, new Production(2, 0, 4)],
+                    [4, 0, new Production(4, 0, 5)],
+                ],
+            );
+        $grammar
+            ->method('getProductionList')
+            ->willReturnMap(
+                [
+                    [1, [new Production(1, 0, 2, 3)]],
+                    [2, [new Production(2, 0, 4)]],
+                    [4, [new Production(4, 0, 5)]],
+                ],
+            );
+        $grammar
+            ->method('getFullProductionList')
+            ->willReturn(
+                [
+                    new Production(1, 0, 2, 3),
+                    new Production(2, 0, 4),
+                    new Production(4, 0, 5),
+                ],
+            );
+        $buffer = new CharBuffer(7);
+        $reader = new TokenReader($buffer, new TokenMatcherByType(), new TokenFactory($grammar));
+        $listener = $this->createStub(AbstractParserListener::class);
+        $parser = new Parser($grammar, $reader, $listener);
+        $parser->loadLookupTable(__DIR__ . '/LookupTable.php');
+
+        $grammar
+            ->expects(self::never())
+            ->method('getNonTerminalList')
+            ->willReturn([1, 2, 4]);
+        $parser->run();
     }
 }
